b3d33aed5b9af4c3554fa37d32e91e804f1b40b1
huatree <wang.yaohua@foxmail.com>
2021-05-17 13:45:31 +0800

    ✨ feat(404模块): 编写页面及业务逻辑

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -3,14 +3,28 @@
 /* 调用模块
 ---------------------------------------------------------------- */
 import React from 'react'
+import { Row, Col,Button } from 'antd'
+
+/* 静态资源
+---------------------------------------------------------------- */
+import './index.less'
 
 /* 唯一的模块导出
 ---------------------------------------------------------------- */
-function ErrorPage() {
+function ErrorPage(props) {
   return (
-    <div>
-      ErrorPage
-    </div>
+    <Row className='not-found'>
+      <Col span={12} className='left'></Col>
+      <Col span={12} className='right'>
+        <h1>404</h1>
+        <h2>抱歉，你访问的页面不存在</h2>
+        <div>
+          <Button type='primary' onClick={() => props.history.replace('/home')}>
+            回到首页
+          </Button>
+        </div>
+      </Col>
+    </Row>
   )
 }
 
