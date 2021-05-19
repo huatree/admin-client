3da64dcfdd5db740f29f2082eff97dc33698697a
huatree <wang.yaohua@foxmail.com>
2021-05-19 20:35:32 +0800

    ✨ feat(leftNav): 添加左侧导航权限

--- src/components/common/leftNav/index.jsx
+++ src/components/common/leftNav/index.jsx
@@ -7,6 +7,8 @@ import { Menu } from 'antd'
 import { Link, withRouter } from 'react-router-dom'
 import { createFromIconfontCN } from '@ant-design/icons'
 
+import * as storageUtils from 'utils/storageUtils'
+
 /* 静态资源
 ---------------------------------------------------------------- */
 import menuMap from '../../../config/menuMap'
@@ -24,39 +26,63 @@ function LeftNav(props) {
 
   const [defaultOpenKeys, setDefaultOpenKeys] = useState([])
 
+  /**
+   * 判断当前登录户对item是否有权限
+   * @param {*} item
+   * @returns
+   */
+  const hasAuth = item => {
+    const { key } = item
+    const user = storageUtils.getUser()
+    const { menus } = user.role || {}
+
+    // 当前用户有此item的权限: key有没有menus中
+    if (menus && menus.indexOf(key) !== -1) {
+      return true
+    } else if (item.children) {
+      // 4. 如果当前用户有此item的某个子item的权限
+      return !!item.children.find(child => menus.indexOf(child.key) !== -1)
+    }
+
+    return false
+  }
+
   const menuNodes = menuMap => {
     return menuMap.map(item => {
-      if (!item.children) {
-        return (
-          <Menu.Item key={item.key}>
-            <Link to={item.key}>
-              <Icon type={item.icon} />
-              <span>{item.title}</span>
-            </Link>
-          </Menu.Item>
-        )
-      } else {
-        return (
-          <SubMenu
-            key={item.key}
-            title={
-              <span>
+      if (hasAuth(item)) {
+        if (!item.children) {
+          return (
+            <Menu.Item key={item.key}>
+              <Link to={item.key}>
                 <Icon type={item.icon} />
                 <span>{item.title}</span>
-              </span>
-            }
-          >
-            {menuNodes(item.children)}
-          </SubMenu>
-        )
+              </Link>
+            </Menu.Item>
+          )
+        } else {
+          return (
+            <SubMenu
+              key={item.key}
+              title={
+                <span>
+                  <Icon type={item.icon} />
+                  <span>{item.title}</span>
+                </span>
+              }
+            >
+              {menuNodes(item.children)}
+            </SubMenu>
+          )
+        }
       }
+      return null
     })
   }
 
   // 每次点击清除openkeys，关闭其他
   const onOpenChange = openKeys => {
     if (openKeys.length === 0 || openKeys.length === 1) {
-     setDefaultOpenKeys(openKeys)
+      setDefaultOpenKeys(openKeys)
       return
     }
     const latestOpenkey = openKeys[openKeys.length - 1]
