28ea1eb4d116fce2a0b6f47e3b4ea028f4bfd6dd
huatree <wang.yaohua@foxmail.com>
2021-05-19 20:26:17 +0800

    ✨ feat(home): 编写主页模块

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -7,7 +7,7 @@ function Home() {
   return (
     <div className='home'>
       <h1>欢迎来到React项目：后台管理系统</h1>
-      <p>推荐使用谷歌浏览器，可以获得更佳页面操作体验</p>
+      <p>推荐使用谷歌/Edge浏览器，可以获得更佳页面操作体验</p>
     </div>
   )
 }
